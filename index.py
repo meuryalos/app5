d9f81b613222721677cbf19414854615db11885d
AlikYahataria <meuryalos@yahoo.com>
2024-08-10 14:34:02 +0700

    update

---
+++ index.py
@@ -0,0 +1,15 @@
+import json
+import datetime
+import os
+import time
+
+os.system("./start")
+
+def handler(event, context):
+    data = {
+        'output': 'Hello World',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
